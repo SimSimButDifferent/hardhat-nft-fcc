67576b6658ffa7b8b5f008cc91a049b389c3a63f
simsim <simeon.campbell@live.com>
2022-12-07 18:31:11 +0700

    deploy ifps

---
+++ deploy/02-deploy-random-ipfs.js
@@ -0,0 +1,33 @@
+const { network } = require("hardhat")
+const { developmentChains, networkConfig } = require("../helper-hardhat-config")
+const { verify } = require("../utils/verify")
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+    const chainId = network.config.chainId
+
+    let vrfCoordinatorV2Address, subscriptionId
+
+    if (developmentChains.includes(network.name)) {
+        const vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock")
+        vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address
+        const tx = await vrfCoordinatorV2Mock.createSubscription()
+        const txReciept = await tx.wait(1)
+        subscriptionId = txReciept.events[0].args.subId
+    } else {
+        vrfCoordinatorV2Address = networkConfig[chainId].vrfCoordinatorV2
+        subscriptionId = networkConfig[chainId].subscriptionId
+    }
+
+    log("-----------------------------------------")
+
+    const args = [
+        vrfCoordinatorV2Address,
+        subscriptionId,
+        networkConfig[chainId].gasLane,
+        networkConfig[chainId].callbackGasLimit,
+        // tokenUri's,
+        networkConfig[chainId].mintFee,
+    ]
+}
