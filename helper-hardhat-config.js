4027016131bb435126d1f4d8a42241c017bfdcdc
simsim <simeon.campbell@live.com>
2022-12-22 13:59:02 +0700

    svg deploy init commit

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -3,6 +3,7 @@ const { ethers } = require("hardhat")
 const networkConfig = {
     5: {
         name: "goerli",
+        ethUsdPriceFeed: "0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e",
         vrfCoordinatorV2: "0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D",
         mintFee: ethers.utils.parseEther("0.01"),
         gasLane: "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15",
